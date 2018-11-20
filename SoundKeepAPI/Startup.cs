f8bc65e44444b1814fb41d787f63180ff8af9112
Jesse <json469@aucklanduni.ac.nz>
2018-11-21 04:25:51 +1300

    Add Swagger

--- SoundKeepAPI/Startup.cs
+++ SoundKeepAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using SoundKeepAPI.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace SoundKeepAPI
 {
@@ -31,6 +32,12 @@ namespace SoundKeepAPI
 
             services.AddDbContext<SoundKeepAPIContext>(options =>
                     options.UseSqlite(Configuration.GetConnectionString("SoundKeepAPIContext")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "SoundKeepAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +54,17 @@ namespace SoundKeepAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty; // launch swagger from root
+            });
         }
     }
 }
